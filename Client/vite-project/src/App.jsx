00ef910134ed18ec7f56b7a8b351861be15ed9f0
fernandamurillo04 <79025794+fernandamurillo04@users.noreply.github.com>
2023-05-03 20:10:51 -0600

    page registrar profesor version 1
    está todo lo de registrar profesor, falta agregar el sidebar

--- Client/vite-project/src/App.jsx
+++ Client/vite-project/src/App.jsx
@@ -2,11 +2,13 @@ import { LoginPage } from './components/LoginPage'
 import { Sidebar } from './components/Sidebar'
 import { ForgotPasswordPage } from './components/ForgotPasswordPage'
 import './index.css'
+import { RegisterProfessorPage } from './components/RegisterProfessorPage'
 
 export default function App(){
   return(
-    <LoginPage />
+    //<LoginPage />
     // <ForgotPasswordPage />
     // <Sidebar />
+    <RegisterProfessorPage/>
   )
 }
