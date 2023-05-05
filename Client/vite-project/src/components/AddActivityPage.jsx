cb54abbbe7abbd550fc9c454c56d8b75e2069c63
Chacalerks <erksmartinez2014@gmail.com>
2023-05-04 23:01:20 -0600

    date picker

--- Client/vite-project/src/components/AddActivityPage.jsx
+++ Client/vite-project/src/components/AddActivityPage.jsx
@@ -67,7 +67,9 @@ export function AddActivityPage(){
                         <label class="block uppercase tracking-wide text-gray-700 text-xs text-right font-bold mb-2" for="grid-fechaFinal">
                             Fecha de Final
                         </label>
-                       
+
+<input type="date" id="start" name="trip-start"
+       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     </div>
                     
                 </div>
