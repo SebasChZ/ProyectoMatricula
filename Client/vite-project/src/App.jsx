b78690fcea92b878f2ff3e388dd77e1401ed8ad5
fernandamurillo04 <79025794+fernandamurillo04@users.noreply.github.com>
2023-05-04 21:12:45 -0600

    falta las fechas
    todo esta menos el sidebar y las fechas

--- Client/vite-project/src/App.jsx
+++ Client/vite-project/src/App.jsx
@@ -3,12 +3,14 @@ import { Sidebar } from './components/Sidebar'
 import { ForgotPasswordPage } from './components/ForgotPasswordPage'
 import './index.css'
 import { RegisterProfessorPage } from './components/RegisterProfessorPage'
+import { AddActivityPage } from './components/AddActivityPage'
 
 export default function App(){
   return(
     //<LoginPage />
     // <ForgotPasswordPage />
     // <Sidebar />
-    <RegisterProfessorPage/>
+   // <RegisterProfessorPage/>
+    <AddActivityPage/>
   )
 }
