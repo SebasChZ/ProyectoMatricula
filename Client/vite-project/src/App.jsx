56d1b32fd848d89f01a1f33ebbfff9cc1550d824
TheCsarbeat <ytcesarjs@gmail.com>
2023-05-05 01:16:12 -0600

    installed some libraries
    For #22

--- Client/vite-project/src/App.jsx
+++ Client/vite-project/src/App.jsx
@@ -1,32 +1,59 @@
-import { Sidebar } from "./components/Sidebar";
 import "./index.css";
+import { Layout } from "./components/Layout";
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "./views/LoginPage";
 import { ForgotPasswordPage } from "./views/ForgotPasswordPage";
-import { createContext } from "react";
-import { useState } from "react";
+import RequireAuth from "./components/RequireAuth";
+import { RegisterProfessorPage } from "./views/RegisterProfessorPage";
+
+const ROLES = {
+  User: 1,
+  Professor: 2,
+  Coordinator: 3,
+  Assistant: 4,
+  AssistantCA: 5,
+};
 
 export default function App() {
-  const [visibility, setVisibility] = useState("invisible");
-
-  function changeVisibility(visibility) {
-    setVisibility(visibility);
-  }
-
   return (
-    <>
-      <div>
-        <LoginPage changeVisibility={changeVisibility} />
-        <Sidebar visibility={visibility} />
-        <div className="sm:ml-[256px]">
-          <Routes>
-            <Route path="/inicio" element={<h1>Inicio</h1>} />
-            <Route path="/planes" element={<h1>Planes</h1>} />
-            <Route path="/estudiantes" element={<h1>Estudiantes</h1>} />
-            <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
-          </Routes>
-        </div>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/*Public routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+
+        {/*Private routes */}
+        {/* User routes */}
+        {/* <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+          <Route path="/user" element={<h1>User</h1>} />
+        </Route> */}
+
+        {/* Professor routes */}
+        {/* <Route element={<RequireAuth allowedRoles={[ROLES.Professor]} />}>
+          <Route path="/professor" element={<h1>Professor</h1>} />
+        </Route> */}
+
+        {/* Coordinator routes */}
+        {/* <Route element={<RequireAuth allowedRoles={[ROLES.Coordinator]} />}>
+          <Route path="/coordinator" element={<h1>Coordinator</h1>} />
+        </Route> */}
+
+        {/* Assistant routes */}
+        {/* <Route element={<RequireAuth allowedRoles={[ROLES.Assistant, ROLES.AssistantCA]} />}>
+          <Route path="/assistant" element={<h1>Assistant</h1>} />
+        </Route> */}
+
+        {/* AssistantCA routes */}
+        <Route element={<RequireAuth allowedRoles={[ROLES.AssistantCA]} />}>
+          <Route
+            path="/registrar-profesor"
+            element={<RegisterProfessorPage />}
+          />
+        </Route>
+
+        {/*Catch all*/}
+        <Route path="*" element={<h1>404 error</h1>} />
+      </Route>
+    </Routes>
   );
 }
--- Client/vite-project/src/components/Sidebar.jsx
+++ Client/vite-project/src/components/Sidebar.jsx
@@ -2,48 +2,46 @@ import logoTec from "../img/logoTECGray.svg";
 import { SidebarContent } from "./SidebarContent";
 import { useContext } from "react";
 
-export function Sidebar({ visibility }) {
+export function Sidebar() {
   return (
     <>
-      <div className={visibility}>
-        <button
-          data-drawer-target="logo-sidebar"
-          data-drawer-toggle="logo-sidebar"
-          aria-controls="logo-sidebar"
-          type="button"
-          class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      <button
+        data-drawer-target="logo-sidebar"
+        data-drawer-toggle="logo-sidebar"
+        aria-controls="logo-sidebar"
+        type="button"
+        class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      >
+        <span class="sr-only">Open sidebar</span>
+        <svg
+          class="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <span class="sr-only">Open sidebar</span>
-          <svg
-            class="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              clip-rule="evenodd"
-              fill-rule="evenodd"
-              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-            ></path>
-          </svg>
-        </button>
+          <path
+            clip-rule="evenodd"
+            fill-rule="evenodd"
+            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+          ></path>
+        </svg>
+      </button>
 
-        <aside
-          id="logo-sidebar"
-          class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-          aria-label="Sidebar"
-        >
-          <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-            <a href="/home" class="flex justify-center  pl-2.5 mb-5">
-              <img src={logoTec} alt="Tec Logo" />
-            </a>
-            <ul class="space-y-2 font-medium">
-              <SidebarContent userType={1} />
-            </ul>
-          </div>
-        </aside>
-      </div>
+      <aside
+        id="logo-sidebar"
+        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar"
+      >
+        <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          <a href="/home" class="flex justify-center  pl-2.5 mb-5">
+            <img src={logoTec} alt="Tec Logo" />
+          </a>
+          <ul class="space-y-2 font-medium">
+            <SidebarContent userType={1} />
+          </ul>
+        </div>
+      </aside>
     </>
   );
 }
