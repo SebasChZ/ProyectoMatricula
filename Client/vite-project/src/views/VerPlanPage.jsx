35ffbdef61287df840beb36aceea6e6f339b18a4
TheCsarbeat <ytcesarjs@gmail.com>
2023-05-12 19:22:22 -0600

    Update VerPlanPage.jsx
    fixed the button and changed dates to span instead of inputs. Closes #33

--- Client/vite-project/src/views/VerPlanPage.jsx
+++ Client/vite-project/src/views/VerPlanPage.jsx
@@ -1,4 +1,5 @@
 import { ActivityCard } from "../components/ActivityCard";
+import { Link } from "react-router-dom";
 
 export function VerPlanPage() {
   return (
@@ -12,13 +13,7 @@ export function VerPlanPage() {
             <h2 className="text-2xl font-normal tracking-tight mb-6">
               Activades
             </h2>
-            <div className="grid grid-cols-2 gap-8 justify-items-center py-6 overflow-y-auto overflow-x-hidden scrollable max-h-[700px] px-6">
-              <ActivityCard />
-              <ActivityCard />
-              <ActivityCard />
-              <ActivityCard />
-              <ActivityCard />
-              <ActivityCard />
+            <div className="grid grid-cols-2 gap-8 justify-items-center py-6 overflow-y-auto overflow-x-hidden scrollable max-h-[600px] px-6">
               <ActivityCard />
               <ActivityCard />
               <ActivityCard />
@@ -26,7 +21,14 @@ export function VerPlanPage() {
               <ActivityCard />
               <ActivityCard />
             </div>
-            <p>Boton</p>
+            <div className="text-left mt-8 h-10">
+              <Link
+                className="text-white font-semibold bg-blue-950 px-6 py-2 rounded-3xl"
+                to="/modificar-plan"
+              >
+                Modificar
+              </Link>
+            </div>
           </div>
           <div className="basis-1/5 mr-20">
             <div className="shadow-xl rounded-md mb-8 p-4">
@@ -38,34 +40,24 @@ export function VerPlanPage() {
             <div>
               <div>
                 <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 text-center"
+                  className="block uppercase tracking-wide text-gray-700 font-bold mb-2 text-center"
                   for="grid-fechaInicio"
                 >
                   Fecha de Inicio
                 </label>
-                <div className="flex items-center">
-                  <input
-                    type="date"
-                    id="start"
-                    name="trip-start"
-                    className="bg-gray-200 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                  />
+                <div className="flex justify-center">
+                  <span>12/02/2023</span>
                 </div>
               </div>
               <div className="mt-4">
                 <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs text-center font-bold mb-2"
+                  className="block uppercase tracking-wide text-gray-700 text-center font-bold mb-2"
                   for="grid-fechaInicio"
                 >
-                  Fecha de Inicio
+                  Fecha Final
                 </label>
-                <div className="flex items-center">
-                  <input
-                    type="date"
-                    id="start"
-                    name="trip-start"
-                    className="bg-gray-200 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                  />
+                <div className="flex justify-center">
+                  <span>26/06/2023</span>
                 </div>
               </div>
             </div>
