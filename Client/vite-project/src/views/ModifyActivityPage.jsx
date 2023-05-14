3f4bf1c2743c8134dac66b02bf480070bda8f647
TheCsarbeat <ytcesarjs@gmail.com>
2023-05-14 14:56:00 -0600

    base design finished

--- Client/vite-project/src/views/ModifyActivityPage.jsx
+++ Client/vite-project/src/views/ModifyActivityPage.jsx
@@ -3,6 +3,7 @@ import chessImg from "../img/chess.png";
 // Components
 import Modal from "../components/Modal";
 import Avatars from "../components/Avatars";
+import DatePicker from "react-multi-date-picker";
 //Hooks
 import { useState } from "react";
 // Icons
@@ -11,7 +12,13 @@ import FileUploadIcon from "@mui/icons-material/FileUpload";
 import AddIcon from "@mui/icons-material/Add";
 
 export function ModifyActivityPage() {
+  const today = new Date();
+  const tomorrow = new Date();
+
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
   const [isOpen, setIsOpen] = useState(false);
+  const [values, setValues] = useState([today, tomorrow]);
 
   function closeModal() {
     setIsOpen(false);
@@ -52,11 +59,11 @@ export function ModifyActivityPage() {
                   rows="6"
                   className="resize-none appearance-none block w-full bg-white-200 text-gray-700 border rounded-2xl py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white shadow-xl"
                 ></textarea>
-                <button className="h-[268px] w-full" onClick={openModal}>
+                <button className="h-[368px] w-full" onClick={openModal}>
                   <img
                     src={chessImg}
                     alt="Afiche"
-                    className="h-[268px] w-full object-none rounded-2xl"
+                    className="h-[368px] w-full object-none rounded-2xl"
                   />
                 </button>
                 <div className="flex justify-between mt-4">
@@ -75,11 +82,11 @@ export function ModifyActivityPage() {
                 </div>
               </div>
               <div className="basis-2/6 flex flex-col items-center">
-                <div>
+                <div className="flex items-center">
                   {/* fechas */}
-                  <div className="w-full px-3 mb-6">
+                  <div className="w-full px-3">
                     <label
-                      className="block uppercase tracking-wide text-gray-700 text-xs text-right font-bold mb-2"
+                      className="block uppercase tracking-wide text-gray-700 text-xs text-center font-bold mb-2"
                       for="grid-fechaInicio"
                     >
                       Fecha de Inicio
@@ -95,7 +102,7 @@ export function ModifyActivityPage() {
                   </div>
                   <div className="w-full px-3">
                     <label
-                      className="block uppercase tracking-wide text-gray-700 text-xs text-right font-bold mb-2"
+                      className="block uppercase tracking-wide text-gray-700 text-xs text-center font-bold mb-2"
                       for="grid-fechaInicio"
                     >
                       Fecha Final
@@ -110,7 +117,7 @@ export function ModifyActivityPage() {
                     </div>
                   </div>
                 </div>
-                <div className="flex items-center mt-4 w-60">
+                <div className="flex items-center mt-4 w-full">
                   {/* selects */}
 
                   <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -125,9 +132,10 @@ export function ModifyActivityPage() {
                         className="appearance-none block w-full bg-white text-gray-700 border rounded-2xl py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white shadow-xl"
                         id="grid-state"
                       >
-                        <option>Hay que cargar profesores</option>
-                        <option>Hay que cargar profesores</option>
-                        <option>Hay que cargar profesores</option>
+                        <option>Orientadora</option>
+                        <option>Motivacional</option>
+                        <option>De apoyo a la vida estudiantil</option>
+                        <option>De orden técnico</option>
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg
@@ -152,9 +160,36 @@ export function ModifyActivityPage() {
                         className="appearance-none block w-full bg-white text-gray-700 border rounded-2xl py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white shadow-xl"
                         id="grid-state"
                       >
-                        <option>Hay que cargar profesores</option>
-                        <option>Hay que cargar profesores</option>
-                        <option>Hay que cargar profesores</option>
+                        <option>Planeada</option>
+                        <option>Notificada</option>
+                        <option>Realizada</option>
+                        <option>Cancelada</option>
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg
+                          className="fill-current h-4 w-4"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label
+                      className="block uppercase tracking-wide text-gray-700 text-xs rounded-lg font-bold mb-2"
+                      for="grid-responsable"
+                    >
+                      Modalidad
+                    </label>
+                    <div className="relative">
+                      <select
+                        className="appearance-none block w-full bg-white text-gray-700 border rounded-2xl py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white shadow-xl"
+                        id="grid-state"
+                      >
+                        <option>Presencial</option>
+                        <option>Virtual</option>
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg
@@ -168,14 +203,30 @@ export function ModifyActivityPage() {
                     </div>
                   </div>
                 </div>
-                <div className="flex justify-between items-center mt-4">
+                <div className="mt-4">
                   {/* responsables */}
-                  <Avatars size={"lg"} />
-                  <button className="text-white border-2 border-blue-400 bg-blue-400 py-1 px-8 rounded-2xl hover:bg-white hover:text-blue-400 ml-4">
-                    <AddIcon />
-                  </button>
+                  <h6 className="block uppercase tracking-wide text-gray-700 text-xs rounded-lg font-bold mb-2 text-center">
+                    Responsables
+                  </h6>
+                  <div className="flex justify-between items-center">
+                    <Avatars size={"lg"} />
+                    <button className="text-white border-2 border-blue-400 bg-blue-400 py-1 px-8 rounded-2xl hover:bg-white hover:text-blue-400 ml-4">
+                      <AddIcon />
+                    </button>
+                  </div>
                 </div>
-                <div>{/* Recordatorios */}</div>
+                <div className="mt-4">
+                  {/* Recordatorios */}
+                  <h6 className="block uppercase tracking-wide text-gray-700 text-xs rounded-lg font-bold mb-2 text-center">
+                    Recordatorios
+                  </h6>
+                  <DatePicker
+                    multiple
+                    value={values}
+                    onChange={setValues}
+                    inputClass="appearance-none block w-full bg-white text-gray-700 border rounded-2xl py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white shadow-xl"
+                  />
+                </div>
               </div>
             </div>
           </form>
